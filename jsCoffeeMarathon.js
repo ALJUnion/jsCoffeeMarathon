a66f42664df44d532d29968d87ab9696aa7cef39
andy <andylouisjean@gmail.com>
2020-03-08 17:17:29 -0400

    coffee marathon push

---
+++ jsCoffeeMarathon.js
@@ -0,0 +1,28 @@
+console.log("coffee-Please");
+
+let coffeeType = "Medium Roast";
+
+console.log(coffeeType);
+
+var latte = 2
+var mocha = 3
+console.log(latte+mocha);
+console.log(latte>mocha);
+console.log(latte<mocha);
+let sugarfree;
+window.alert("sugar free")
+
+let customer1 = "Kamala khan", drinkChoice = "Chai",quantity = "4", foodAllergy = false;
+console.log(customer1+ " "+drinkChoice+ " "+quantity+ " "+foodAllergy);
+
+let chai = 2.50
+console.log(chai+quantity);
+
+//  chai = 2;
+// var chai = 2;
+console.log(chai);
+
+let total = drinkChoice+" "+quantity+" "+"$"+quantity * chai;
+
+alert("Your Final Order")
+"9"+""
